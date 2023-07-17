21d6242680ff3e3828b9162bcc14b740403387f5
yaozm <ityaozm@gmail.com>
2023-07-17 18:36:15 +0800

    feat(create-github-release-worker): add CreateGithubReleaseWorker class
    - Add CreateGithubReleaseWorker class
    - Implement ReleaseWorkerInterface
    - Initialize ProcessRunner in constructor
    - Implement work() method
    - Implement getDescription() method

---
+++ src/CreateGithubReleaseWorker.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/monorepo-builder-worker.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\MonorepoBuilderWorker;
+
+use PharIo\Version\Version;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
+use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+
+class CreateGithubReleaseWorker implements ReleaseWorkerInterface
+{
+    /** @var ProcessRunner */
+    private $processRunner;
+
+    public function __construct(ProcessRunner $processRunner)
+    {
+        $this->processRunner = $processRunner;
+    }
+
+    public function work(Version $version): void
+    {
+    }
+
+    public function getDescription(Version $version): string
+    {
+        return sprintf('Create github "%s" release', $version->getVersionString());
+    }
+}
