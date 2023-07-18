72c5551c8a7dfd2ba3b914043056e7a75072fd36
yaozm <ityaozm@gmail.com>
2023-07-18 18:19:51 +0800

    refactor(ReleaseWorker): update createProcessRunner method
    - Update the createProcessRunner method in the ReleaseWorker class
    - Set the verbosity level of ConsoleOutput to VERBOSITY_VERY_VERBOSE

--- src/ReleaseWorker.php
+++ src/ReleaseWorker.php
@@ -15,6 +15,7 @@ namespace Guanguans\MonorepoBuilderWorker;
 use Guanguans\MonorepoBuilderWorker\Contract\CheckReleaseWorkerEnvironmentInterface;
 use MonorepoBuilderPrefix202304\Symfony\Component\Console\Input\ArgvInput;
 use MonorepoBuilderPrefix202304\Symfony\Component\Console\Output\ConsoleOutput;
+use MonorepoBuilderPrefix202304\Symfony\Component\Console\Output\OutputInterface;
 use MonorepoBuilderPrefix202304\Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
@@ -23,6 +24,9 @@ abstract class ReleaseWorker implements CheckReleaseWorkerEnvironmentInterface, 
 {
     protected static function createProcessRunner(?SymfonyStyle $symfonyStyle = null): ProcessRunner
     {
-        return new ProcessRunner($symfonyStyle ?: new SymfonyStyle(new ArgvInput(), new ConsoleOutput()));
+        return new ProcessRunner($symfonyStyle ?: new SymfonyStyle(
+            new ArgvInput(),
+            new ConsoleOutput(OutputInterface::VERBOSITY_VERY_VERBOSE)
+        ));
     }
 }
