e33355182158826bae8d0d759a5b641b0396e8b1
yaozm <ityaozm@gmail.com>
2024-01-07 14:54:29 +0800

    chore(env): Update fix-namespace-prefix command
    - Update fix-namespace-prefix command in composer.json
    - Change visibility of fixNamespacePrefix() method in EnvironmentChecker.php

--- src/Support/EnvironmentChecker.php
+++ src/Support/EnvironmentChecker.php
@@ -57,7 +57,7 @@ class EnvironmentChecker
         }
     }
 
-    public static function fixNamespacePrefix(): void
+    protected static function fixNamespacePrefix(): void
     {
         $yearMonth = date('Ym');
         while ($yearMonth >= 202310 && ! class_exists(sprintf('MonorepoBuilderPrefix%s\Symfony\Component\Console\Style\SymfonyStyle', $yearMonth))) {
