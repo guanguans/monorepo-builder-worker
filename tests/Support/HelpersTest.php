f965e6272efb86e3a8253ec709605031969c5112
guanguans <ityaozm@gmail.com>
2025-04-06 23:38:42 +0800

    test(Helpers): Add test for classes retrieval
    - Create a test case for the `classes` function
    - Ensure it returns an array and is truthy
    
    This test verifies that the `classes` function behaves as expected, helping to catch potential issues in future changes.

---
+++ tests/Support/HelpersTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @noinspection AnonymousFunctionStaticInspection */
+/** @noinspection NullPointerExceptionInspection */
+/** @noinspection PhpPossiblePolymorphicInvocationInspection */
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection StaticClosureCanBeUsedInspection */
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/monorepo-builder-worker
+ */
+
+namespace Guanguans\MonorepoBuilderWorkerTests\Support;
+
+use function Guanguans\MonorepoBuilderWorker\Support\classes;
+
+it('can get classes', function (): void {
+    expect(classes())->toBeArray()->toBeTruthy();
+})->group(__DIR__, __FILE__);
