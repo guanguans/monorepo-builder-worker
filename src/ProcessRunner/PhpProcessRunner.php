700e6696dda86d5dd9b89e7b7c8738ec8c6b7023
guanguans <ityaozm@gmail.com>
2026-04-05 22:56:30 +0800

    feat(release-worker): Add BuildLaravelZeroAppReleaseWorker and PhpSubprocessRunner
    - Introduce BuildLaravelZeroAppReleaseWorker to automate building Laravel Zero app releases.
    - Implement static property for app name and methods for checking environment, setting app name, and building app.
    - Add static method to locate composer executable and run composer commands.
    - Create PhpSubprocessRunner for running PHP subprocesses with Symfony Console integration.
    - Handle process failures and provide verbose output during command execution.
    - Enhance release workflow by ensuring built app version matches expected version.
    - Provide improved error handling and reporting for subprocess execution.

---
+++ src/ProcessRunner/PhpProcessRunner.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2026 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/monorepo-builder-worker
+ */
+
+namespace Guanguans\MonorepoBuilderWorker\ProcessRunner;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\PhpProcess;
+
+/**
+ * @see \Symplify\MonorepoBuilder\Release\Process\ProcessRunner
+ */
+final readonly class PhpProcessRunner
+{
+    /** Reasonable timeout to report hang off: 10 minutes. */
+    private const TIMEOUT = 600;
+
+    public function __construct(private SymfonyStyle $symfonyStyle) {}
+
+    public function run(string $script, ?string $cwd = null): string
+    {
+        $phpProcess = $this->createPhpProcess($script, $cwd);
+
+        if ($this->symfonyStyle->isVerbose()) {
+            $this->symfonyStyle->note("Running phpProcess: {$phpProcess->getCommandLine()}");
+        }
+
+        $phpProcess->run();
+
+        $this->reportResult($phpProcess);
+
+        return $phpProcess->getOutput();
+    }
+
+    private function createPhpProcess(string $script, ?string $cwd): PhpProcess
+    {
+        return new PhpProcess($script, $cwd, null, self::TIMEOUT);
+    }
+
+    private function reportResult(PhpProcess $phpProcess): void
+    {
+        if ($phpProcess->isSuccessful()) {
+            return;
+        }
+
+        throw new ProcessFailedException($phpProcess);
+    }
+}
---
+++ src/ReleaseWorker/BuildLaravelZeroAppReleaseWorker.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2026 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/monorepo-builder-worker
+ */
+
+namespace Guanguans\MonorepoBuilderWorker\ReleaseWorker;
+
+use Guanguans\MonorepoBuilderWorker\ProcessRunner\PhpSubprocessRunner;
+use PharIo\Version\Version;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\ExecutableFinder;
+
+final class BuildLaravelZeroAppReleaseWorker extends AbstractReleaseWorker
+{
+    private static ?string $appName = null;
+
+    public function __construct(private readonly PhpSubprocessRunner $phpSubprocessRunner) {}
+
+    #[\Override]
+    public static function check(): void
+    {
+        \assert(
+            \is_string(self::$appName),
+            \sprintf('The property "%s::$appName" must be set by calling the method "setAppName".', self::class)
+        );
+        self::createPhpSubprocessRunner()->run(['-v']);
+        self::createPhpSubprocessRunner()->run([self::$appName, '--version', '--ansi', '-v']);
+        self::createPhpSubprocessRunner()->run([self::findComposer(), '--version', '--ansi', '-v']);
+    }
+
+    /**
+     * @api
+     *
+     * @param non-empty-string $appName
+     */
+    public static function setAppName(string $appName): void
+    {
+        self::$appName = $appName;
+    }
+
+    public function getDescription(Version $version): string
+    {
+        return \sprintf('Build app "%s" version "%s"', self::$appName, $version->getOriginalString());
+    }
+
+    public function work(Version $version): void
+    {
+        register_shutdown_function(function (): void {
+            $this->phpSubprocessRunner->run([self::findComposer(), 'install', '--ansi', '-v']);
+            $this->phpSubprocessRunner->run([self::$appName, '--version', '--ansi', '-v']);
+        });
+        $this->phpSubprocessRunner->run([self::findComposer(), 'install', '--no-dev', '--no-scripts', '--ansi', '-v']);
+        $this->phpSubprocessRunner->run([self::$appName, 'app:build', self::$appName, '--build-version', $version->getOriginalString(), '--ansi']);
+        \assert(
+            str_contains(
+                $this->phpSubprocessRunner->run([\sprintf('builds/%s', self::$appName), '--version', '--no-ansi']),
+                $version->getOriginalString()
+            ),
+            \sprintf('Build app "%s" failed.', self::$appName)
+        );
+    }
+
+    public static function createPhpSubprocessRunner(?SymfonyStyle $symfonyStyle = null): PhpSubprocessRunner
+    {
+        static $phpSubprocessRunner;
+
+        if (!$phpSubprocessRunner instanceof PhpSubprocessRunner || $symfonyStyle instanceof SymfonyStyle) {
+            $phpSubprocessRunner = new PhpSubprocessRunner($symfonyStyle ?? self::createSymfonyStyle());
+        }
+
+        return $phpSubprocessRunner;
+    }
+
+    /**
+     * @see \Illuminate\Console\Application::artisanBinary()
+     * @see \Illuminate\Console\Application::phpBinary()
+     * @see \Illuminate\Support\Composer::findComposer()
+     *
+     * @noinspection PhpUndefinedNamespaceInspection
+     */
+    private static function findComposer(): string
+    {
+        static $composer;
+
+        return $composer ??= (new ExecutableFinder)->find('composer', 'composer');
+    }
+}
